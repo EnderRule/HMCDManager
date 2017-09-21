4785659cbc168c89b1ffb14f12005130cef8d651
HuangZhongQing <hzqmac@HuangZhngdeiMac.lan>
2017-09-21 17:48:51 +0800

    init commit

---
+++ HMCDManager/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  HMCDManager
+//
+//  Created by HuangZhongQing on 2017/9/21.
+//  Copyright © 2017年 HuangZhongQing. All rights reserved.
+//
+
+import UIKit
+
+class Message: NSManagedObject {
+
+}
